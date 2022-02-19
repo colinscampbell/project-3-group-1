75dbf154c6991f35411b628ea5b7d198922c3bf2
EtienneDembele <dembeleetienne525@yahoo.com>
2022-02-19 13:47:08 -0500

    updated app.py

--- app.py
+++ app.py
@@ -24,10 +24,21 @@ Base = automap_base()
 Base.prepare(engines, reflect=True)
 session = Session(conn)
 Base.classes.keys()
-
+# Performs database schema inspection
+#insp = sqlalchemy.inspect(engines)
+#print(insp.get_table_names())
 Wtr = Base.classes.Weather_Raw
 St_Cd = Base.classes.State_Code
 join = session.query( Wtr , St_Cd ).filter(Wtr.State == St_Cd.Code).statement
-
 df = pd.read_sql_query(join, session.bind)
-df
+#print(df)
+
+States_list = [2, 3, 4, 8, 11, 18, 30, 45, 41,42]
+df1 = df.loc[((df['Year'] >= 2012) & (df['Year'] <= 2021) & (df['State'].isin(States_list)))] ### this is where put the 10 states and the years that we are planning to use
+df1 = df1.drop(['Element' , 'County', 'Code'] ,  axis=1)
+
+df_avg = round(df1.groupby(['State_1', 'Year']).mean(), 2)
+print(df_avg)
+# Sending output to a CSV file to get the full picture
+#df_avg.to_csv(r'C:\Users\georg\project-3-group-1\Datasetsr', index=True, header=True)
+session.close()
