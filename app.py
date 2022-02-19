37ae5039b8d976e5a96d737c96d23203cddeaad7
Jorgehdzdata <georgo5573@gmail.com>
2022-02-18 21:33:10 -0600

    Updates made to app.py and test_code under my branch

--- app.py
+++ app.py
@@ -24,10 +24,18 @@ Base = automap_base()
 Base.prepare(engines, reflect=True)
 session = Session(conn)
 Base.classes.keys()
-
+# Performs database schema inspection
+#insp = sqlalchemy.inspect(engines)
+#print(insp.get_table_names())
 Wtr = Base.classes.Weather_Raw
 St_Cd = Base.classes.State_Code
 join = session.query( Wtr , St_Cd ).filter(Wtr.State == St_Cd.Code).statement
-
 df = pd.read_sql_query(join, session.bind)
 print(df)
+
+df1 = df.loc[((df['State'] == 2 ) & (df['Year'] == 1895) )] ### this is where put the 10 states and the years that we are planning to use
+df1 = df1.drop(['Element' , 'County', 'Code' ,'State'] ,  axis=1)
+
+df_avg = round(df1.groupby(['State_1', 'Year']).mean(), 2)
+print(df_avg)
+session.close()
